b350b3123ea72ee2b24f7136e3ade07b1be3610c
Sophie <Sophie@sfr.fr>
2025-04-13 13:11:25 +0200

    reactroutter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../styles/App.css'
+import logementsData from '../logement.json'
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  
   return (
     <>
+    
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -24,6 +27,20 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+      </div>
+      <div className="logements-list">
+        {logementsData.map((logement) => (
+          <div key={logement.id} className="logement-card">
+            <h2>{logement.title}</h2>
+            <img src={logement.cover} alt={logement.title} />
+            <p>{logement.description}</p>
+            <div className="pictures">
+              {logement.pictures.map((picture, index) => (
+                <img key={index} src={picture} alt={`Logement image ${index + 1}`} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,15 +7,24 @@ import {
 } from "react-router-dom";
 import Root from "../routes/root.jsx";
 import '../styles/index.css'
-//import { StrictMode } from 'react'
+import App from './App.jsx'
+
+import { StrictMode } from 'react'
 //import { createRoot } from 'react-dom/client'
-//import App from './components/App.jsx'
+
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children :[
+      {
+        path:"home",
+        element : <App />,
+      }
+    ]
   },
 ]);
 
