032772bfe626cde2b44c5b512bffac5816d011f5
Sophie <Sophie@sfr.fr>
2025-05-06 17:38:34 +0200

    changement du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import "../styles/Footer.scss";
-import logo_footer from "../assets/logo_footer.png";
-import logo_footer_mobile from "../assets/logo_footer_mobile.png";
+import logo from "../assets/logo-footer.png"
+
 
 function Footer(){
     return (
-        <footer>
-            <img src={logo_footer} alt="logo kasa" className="kasa-logo-footer"></img>
-            <img src={logo_footer_mobile} alt="logo kasa" className="kasa-logo-footer-mobile"></img>
+        <footer className="footer">
+            <img src={logo} alt='logo kasa' className="kasa-logo-footer"></img>
+            <p> © 2020 Kasa. All rights reserved </p>
         </footer>
     )
 }
