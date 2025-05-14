ebb146c8457dac8fb61b3e8c71cd18c5cd79d7d2
Sophie <Sophie@sfr.fr>
2025-05-14 11:50:43 +0200

    page finie

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,11 +5,13 @@ import Header from "../components/Header.jsx";
 function ErrorPage(){
   return (
     <>
+    <div className="container-error">
         <Header />
-        <div id="error-page">
-            <h1> 404 </h1>
-            <p>Oups! La page que vous demandez n'existe pas.</p>
-            <a href="/home" class="lien-retour-accueil">Retourner sur la page d’accueil</a>
+            <div id="error-page">
+                <h1> 404 </h1>
+                <p className="error-para">Oups! La page que vous demandez n'existe pas.</p>
+                <a href="/home" class="lien-retour-accueil">Retourner sur la page d’accueil</a>
+            </div>
         </div>
         <Footer />
     </>
