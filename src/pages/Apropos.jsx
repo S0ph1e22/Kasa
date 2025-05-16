d238085959e27b0232d52a2fa9ef550ac5d91e9a
Sophie <Sophie@sfr.fr>
2025-05-16 14:17:50 +0200

    creation d'un json avec les données de la page a propos, fonctionnel

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -2,19 +2,25 @@ import '../styles/Apropos.scss';
 import Collabs from "../components/Collabs.jsx"
 import Banner from "../components/Banner.jsx";
 import banner_a_propos from "../assets/banner-a-propos.png";
+import aProposData from "../aPropos.json";
 
 function APropos(){
+
 return(
     <>
         <Banner image={banner_a_propos} />
-        <div className='txt-collabs'>
-            <Collabs textTitle="Fiabilité " textCollabs="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
-            <Collabs textTitle="Respect" textCollabs="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-            <Collabs textTitle="Service" textCollabs="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
-            <Collabs textTitle="Sécurité" textCollabs="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
-        </div>
-    </>
-)
+        <div className='txt-collabs'>   {/* div qui contient les collabs */}
+            {/* parcours tt les objets du tableau apropos, item = 'une case', creation composant collabs avec le titre, txt et id*/}
+                {aProposData.map((item) => ( 
+                    <Collabs 
+                        key={item.id} 
+                        textTitle={item.title} 
+                        textCollabs={item.texte} 
+                    />
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default APropos
